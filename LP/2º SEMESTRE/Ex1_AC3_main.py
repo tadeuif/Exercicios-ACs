fac3cd3a2746ebf453bca2271415d2ac096cf7b1
Tadeu <tadeuif@hotmail.com>
2018-08-29 23:53:50 -0300

    UPDATE

--- LP/2º SEMESTRE/Ex1_AC3_main.py
+++ LP/2º SEMESTRE/Ex1_AC3_main.py
@@ -3,3 +3,10 @@ tv = Televisao()
 print(tv.ligada)
 print(tv.canal)
 
+tv_sala = Televisao()
+tv_sala.ligada = True
+tv_sala.canal = 5
+print(tv.ligada)
+print(tv.canal)
+print(tv_sala.ligada)
+print(tv_sala.canal)
