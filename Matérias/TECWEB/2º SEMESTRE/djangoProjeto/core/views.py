228b050d0b716fd26a6b4ffaaf456efd21605847
tadeuif <tadeuif@hotmail.com>
2018-10-15 11:37:48 -0300

    UPDATE DJANGO

--- Matérias/TECWEB/2º SEMESTRE/djangoProjeto/core/views.py
+++ Matérias/TECWEB/2º SEMESTRE/djangoProjeto/core/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Olá mundo!")
+	context = {
+		'title': 'E-Commerce FIT',
+		'paragrafo1': 'Primeiro parágrafo',
+		'paragrafo2': 'Segundo parágrafo'
+	}
+	return render(request, "index.html", context)
+
+def login(request):
+	if request.method == "GET":
+		print("Acesso via GET")
+	else:
+		print("Acesso via POST")
+	return render(request, "login.html")
