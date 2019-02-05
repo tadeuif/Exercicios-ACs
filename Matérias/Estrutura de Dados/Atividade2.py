204aee7b55ec52078cb1338f76d746c3dd0e72ca
Tadeu Inocencio Freitas <1800250@SALAS.AULAS>
2019-02-05 11:32:21 -0200

    COMMIT

--- Matérias/Estrutura de Dados/Atividade2.py
+++ Matérias/Estrutura de Dados/Atividade2.py
@@ -7,7 +7,15 @@ while nomes != '@':
 print(lista)
 print(len(lista))
 
-i = 0
+#i = 0
 
-for l in lista[i]:
-    print(len(i))
+#for l in lista[i]:
+#    print(len(l))
+
+max_tamanho = 0
+for i,nome in enumerate(lista): #DEVOLVE INDICE E ELEMENTO
+    if len(nome) > max_tamanho:
+        max_tamanho = len(nome)
+        max_nome = nome
+        max_i = i
+print("Maior nome: %s, com %d letras, na posição %d"%(max_nome, max_tamanho, max_i+1))
