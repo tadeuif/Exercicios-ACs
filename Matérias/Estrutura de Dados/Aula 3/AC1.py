72adf6545c6c49b4418bce180244906ffaa9e409
Tadeu <tadeuif@hotmail.com>
2019-02-24 14:15:58 -0300

    UPDATE AC1 EX1, 2 ,3

--- Matérias/Estrutura de Dados/Aula 3/AC1.py
+++ Matérias/Estrutura de Dados/Aula 3/AC1.py
@@ -51,11 +51,57 @@ def verifica_sequencia(seq):
             return False
     return True
 
+def verifica_circular(seq):
+    it = 0
+    it2 = 1
+    indtup = 0
+    il = 0
+    il2 = 1
+    lenght = len(seq) - 1
+    peca3 = Domino(0,0)
+    for i in range(lenght):
+        peca1 = Domino(seq[il][it],seq[il][it2])
+        peca2 = Domino(seq[il2][it],seq[il2][it2])
+        if peca1.connecta(peca2):
+            if indtup < 1:
+                peca3 = peca1
+                seq[indtup]
+                indtup += 1
+                il += 1
+                il2 += 1
+            else:
+                seq[indtup]
+                indtup += 1
+                il += 1
+                il2 += 1
+        else:
+            return False
+    if peca2.connecta(peca3):
+        return True
+    else:
+        return False
+
+def verifica_repetidas(seq):
+    lenght = len(seq) - 1
+    indtup = 0
+    indtup2 = 1
+    for i in range(lenght):
+        if indtup < indtup2:
+            for indlista in range(lenght):
+                if seq[indtup] == seq[indtup2]:
+                    return True
+                else:
+                    indtup2 += 1
+        indtup += 1
+        indtup2 = indtup + 1
+        lenght -= 1
+    return False
 
 pecas = ler_arquivo_todo('domino.dat')
 print(pecas)
 print(verifica_sequencia(pecas))
-
+print(verifica_circular(pecas))
+print(verifica_repetidas(pecas))
 #peca1 = Domino(pecas[0][0],pecas[0][1])
 #peca2 = Domino(pecas[1][0],pecas[1][1])
 #print(peca1.lado_a())
