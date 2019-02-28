013d2ae4a6a86c9275204ac97930c484e0dfa836
Tadeu Inocencio Freitas <1800250@salas.aulas>
2019-02-28 12:05:44 -0300

    Changes

--- Matérias/DAD/Aula 3/funcoes_banco/estruturado/MeusModulos/ContaCorrente.py
+++ Matérias/DAD/Aula 3/funcoes_banco/estruturado/MeusModulos/ContaCorrente.py
@@ -1,13 +1,35 @@
+from Clientes import *
+
 class LimiteCreditoExcedidoException(Exception):
     pass #nao precisa fazer nada nessa classe, ela ja define a excessao nova
 
 #crie aqui uma nova excessao LimiteTransferenciaExcedidoException
+class LimiteTransferenciaExcedidoException(Exception):
+    pass
 
 def credito(valor, cliente, limite_credito, limite_transferencia):
-    pass # crie aqui a funcao credito
+    cliente['saldo'] = valor + cliente['saldo']
 
 def debito(valor, cliente, limite_credito, limite_transferencia):
-    pass # crie aqui a funcao debito
+    saque_max = cliente['saldo'] + limite_credito
+    if valor > saque_max:
+        #print("Sem saldo suficiente")
+        raise LimiteCreditoExcedidoException
+    cliente['saldo'] = cliente['saldo'] - valor
 
 def transferencia(lista_clientes,id_cliente_doador,id_cliente_receptor,valor,limite_credito,limite_transferencia):
-    pass # crie aqui a funcao transferencia
+    saldo_doador = id_cliente_doador['saldo'] - valor
+    saldo_receptor = id_cliente_receptor['saldo'] + valor
+
+    cliente_doador = lista_clientes.pesquisar(lista_clientes, id_cliente_doador)                #lista_clientes[id_cliente_doador]
+    cliente_receptor = lista_clientes.pesquisar(lista_clientes, id_cliente_receptor)                                                                   #lista_clientes[id_cliente_receptor]
+    
+    todos_indices = list(range(len(lista_clientes)))
+
+    for i in todos_indices:
+       if cliente_doador == lista_clientes[i]:
+           cliente_doador['saldo'] = saldo_doador
+    
+    for i in todos_indices:
+       if cliente_receptor == lista_clientes[i]:
+           cliente_receptor['saldo'] = saldo_receptor
