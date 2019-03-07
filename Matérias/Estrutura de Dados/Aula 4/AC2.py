a4ec2b6ea7c39a2e7d9a8a4184158d0358ff8063
Tadeu <tadeuif@hotmail.com>
2019-03-07 00:45:13 -0300

    AC2 ED IMCOMPLETA

--- Matérias/Estrutura de Dados/Aula 4/AC2.py
+++ Matérias/Estrutura de Dados/Aula 4/AC2.py
@@ -7,20 +7,36 @@ def push(p, elemento, tipo=str):
 def pop(p):
     return p.pop() if len(p) > 0 else None
 
-def len(p):
+def lenght(p):
     return len(p)
 
-s = 'arara'
-lista = list(s)
+#s = '(1 + 2)*5', '3*((1*2)+3)'
+#lista = list(s)
 
-for c in lista:
-    push(pilha, c)
+def palindromo(string):
 
-def palindromo(pilha):
-    
+    lista = list(string)
+    for c in lista:
+        push(pilha, c)
+
     if pilha == pilha[::-1]:
         return True
     else:
         return False       
 
-print(palindromo(pilha))
+#print(palindromo('arara'))
+
+def balanceada(string):
+    lista = list(string)
+    for c in lista:
+        if c == "(" or c == ")" or c == "[" or c == "]" or c == "{" or c == "}":
+            push(pilha, c)
+        else:
+            pass
+
+    if pilha == pilha[::-1]: #Não é isso!
+        return True
+    else:
+        return False 
+
+print(balanceada('(1234)'))
