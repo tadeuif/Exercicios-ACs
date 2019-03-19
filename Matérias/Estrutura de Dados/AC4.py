f6ba295491af7f2b3c4e6512a1d96280d85f6eeb
Tadeu Inocencio Freitas <1800250@SALAS.AULAS>
2019-03-19 10:17:57 -0300

    Changes

--- Matérias/Estrutura de Dados/AC4.py
+++ Matérias/Estrutura de Dados/AC4.py
@@ -1,19 +1,4 @@
-def verifica_ordenacao(lista):
-    lenght = len(lista)
-    ind_2 = 1
-    for i in range(lenght):
-        if ind_2 < lenght:
-            if lista[i] > lista[ind_2]:
-                return False
-            else:    
-                ind_2 += 1
-    else:
-        return True
-
-lista = [0,1,2,3,4,5,6,7,8]
 lista2 = [100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142,144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200]               
-
-print(verifica_ordenacao(lista))
 
 def busca(lista, elemento_desejado):
     for i in lista:
@@ -22,9 +7,6 @@ def busca(lista, elemento_desejado):
     return None
 
 print(busca(lista2, 122))
-"""Busca linear que retorna o índice do elemento desejado se estiver na lista, caso contrário retorna None"""
-    
-
 
 def busca_binaria(lista, elemento_desejado):
     tamanho = len(lista)
@@ -47,13 +29,3 @@ def busca_binaria(lista, elemento_desejado):
                 elemento_central = int((elemento_inicial + elemento_final) / 2)
 
 print(busca_binaria(lista2, 122))
-        
-
-
-"""Busca binária que retorna o índice do elemento desejado se estiver na lista, caso contrário retorna None"""
-    
-
-
-#def busca_binaria_strings(lista, elemento_desejado):
-"""Busca binária com uma lista de strings que retorna o índice do elemento desejado se estiver na lista, caso contrário retorna None. Se a lista não estiver ordenada, também retorna None"""
-    #pass'''
--- Matérias/Estrutura de Dados/Eu.py
+++
@@ -1,8 +0,0 @@
-lista = []
-def acres(lista, valor):
-    while valor <= 200:
-        lista.append(valor)
-        valor += 2
-    return lista
-
-print(acres(lista, 100))
