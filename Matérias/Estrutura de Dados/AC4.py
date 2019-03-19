77c8db96bb784f4c7d4536e2b1e0c7f8241f4db4
Tadeu Inocencio Freitas <1800250@SALAS.AULAS>
2019-03-19 10:29:16 -0300

    Changes

--- Matérias/Estrutura de Dados/AC4.py
+++ Matérias/Estrutura de Dados/AC4.py
@@ -1,7 +1,8 @@
 lista2 = [100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142,144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200]               
 
 def busca(lista, elemento_desejado):
-    for i in lista:
+    lenght = len(lista)
+    for i in range(lenght):
         if elemento_desejado == lista[i]:
             return i
     return None
