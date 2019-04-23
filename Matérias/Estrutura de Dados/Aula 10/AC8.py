dc6d5c2e20cd5a77b308784645dee193c3e6c97c
Tadeu Inocencio Freitas <1800250@SALAS.AULAS>
2019-04-23 12:21:55 -0300

    AC8 EST DADOS

--- Matérias/Estrutura de Dados/Aula 10/AC8.py
+++ Matérias/Estrutura de Dados/Aula 10/AC8.py
@@ -28,18 +28,6 @@ def organizar_estados(estados):
 estados_organizados = organizar_estados(estados)
 def cidades_por_estado(cidades):
     d = {}
-    lista_cidades = []
-    cont_UFS = 0
-    tamanho_lista_cidades = len(cidades)
-    tam_cidades_organizadas = len(estados_organizados)
-    '''
-    for estado in range(tam_cidades_organizadas):
-        for i in range(tamanho_lista_cidades):
-            if estados_organizados[estado] not in d:
-                d = {estados_organizados[estado]: lista_cidades.append(cidades[i])}
-            elif estados_organizados[estado] == d[estado]:
-                lista_cidades.append(cidades[i])
-    '''
     for cidade, estado in zip(cidades, estados):
         if estado not in d:
             d[estado] = [cidade]
