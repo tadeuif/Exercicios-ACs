fee1fe045722b2dce05be3aa00b9c01441c667f2
Tadeu Inocencio Freitas <1800250@salas.aulas>
2019-04-29 10:46:15 -0300

    EST DADOS

--- Matérias/Estrutura de Dados/Aula 10/AC8.py
+++ Matérias/Estrutura de Dados/Aula 10/AC8.py
@@ -1,17 +1,19 @@
-'''
-Tadeu Inocencio Freitas - RA 1800250
-Tibério Cruz - RA 1800110
-
-'''
-
 import pandas as pd
 import json
 
 data = pd.read_csv("cidades_br.csv")
+cidade = data['Nome do Município']
+ind = cidade[1]
+#print(data)
+
+muni_cid = data[['Nome do Município', 'Estado']]
+#print(muni_cid)
 
 cidades = data['Nome do Município'].tolist()
 estados = data['UF'].tolist()
 
+
+#print(estados)
 def organizar_estados(estados):
     tamanho_lista_estados = len(estados)
     lista_UF_n_duplicados = []
@@ -23,6 +25,7 @@ def organizar_estados(estados):
             pass
     return lista_UF_n_duplicados
 
+#print(organizar_estados(estados))
 estados_organizados = organizar_estados(estados)
 def cidades_por_estado(cidades):
     d = {}
@@ -32,6 +35,8 @@ def cidades_por_estado(cidades):
         elif estado in d:
             d[estado] += [cidade]
     return d
+
+#print(cidades_por_estado(cidades))
 
 lista_alfa = [chr(ord('A')+i) for i in range(26)]
 
@@ -45,12 +50,16 @@ def cidades_por_letra(cidades):
                 d[i] = lista_cidades
     return d
 
+#print(cidades_por_letra(cidades))
+
 def estado_por_cidade(cidades):
     d = {}
     for cidade, estado in zip(cidades, estados):
         if cidade not in d:
             d[cidade] = estado
     return d
+
+#print(estado_por_cidade(cidades))
 
 def arvore_do_brasil(cidades):
     brasil = {}
@@ -62,10 +71,19 @@ def arvore_do_brasil(cidades):
     d = {'Brasil': brasil}    
     return d
 
+#print(arvore_do_brasil(cidades))
+
 brasa = arvore_do_brasil(cidades)
 cid = cidades_por_estado(cidades)
 cid_letra = cidades_por_letra(cidades)
 estado_cid = estado_por_cidade(cidades)
+data = {}
+data['test'] = 'test'
+#json_dicto = json.loads(json_file)
+#print(json_file['GO'])
+#json_dict = json.dumps(data)
+
+#print(json_dict)
 
 with open('teste.json', 'w') as json_file:
   json.dump(brasa, json_file)
