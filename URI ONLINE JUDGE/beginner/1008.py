329a7033ef11314bf5428a2ddfa0ba8a5288a4db
Tadeu Inocencio <tadeuif@hotmail.com>
2020-05-12 13:18:04 -0300

    erased

--- URI ONLINE JUDGE/beginner/1008.py
+++
@@ -1,6 +0,0 @@
-NUMBER = int(input())
-WORKED_HOURS = int(input())
-PER_HOUR_AMOUNT = float(input())
-SALARY = WORKED_HOURS * PER_HOUR_AMOUNT
-print("NUMBER = %i" %NUMBER)
-print("SALARY = U$ %.2f" %SALARY)
